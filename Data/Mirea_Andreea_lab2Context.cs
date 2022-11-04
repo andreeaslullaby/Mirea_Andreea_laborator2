82b947824b77e6a84badb8aed0d765ea8f12cdc5
Asus <Asus@DESKTOP-TS8Q3BN>
2022-11-04 17:11:39 +0200

    Add project files.

---
+++ Data/Mirea_Andreea_lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mirea_Andreea_lab2.Models;
+
+namespace Mirea_Andreea_lab2.Data
+{
+    public class Mirea_Andreea_lab2Context : DbContext
+    {
+        public Mirea_Andreea_lab2Context (DbContextOptions<Mirea_Andreea_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mirea_Andreea_lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mirea_Andreea_lab2.Data;
+using Mirea_Andreea_lab2.Models;
+
+namespace Mirea_Andreea_lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Mirea_Andreea_lab2.Data.Mirea_Andreea_lab2Context _context;
+
+        public IndexModel(Mirea_Andreea_lab2.Data.Mirea_Andreea_lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.ToListAsync();
+            }
+        }
+    }
+}
